e1e48e2033c5542023b3d6181564d9a016786559
Irving Lop <ilopezp@gbm.com.mx>
2020-05-14 16:18:58 -0500

    [Udacity] Lesson 3 App Navigation - 7. Adding TitleFragment

---
+++ app/src/main/java/dev/irving/portfolioapp/trivia/TitleFragment.kt
@@ -0,0 +1,27 @@
+package dev.irving.portfolioapp.trivia
+
+import android.os.Bundle
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import androidx.databinding.DataBindingUtil
+import androidx.fragment.app.Fragment
+import dev.irving.portfolioapp.R
+import dev.irving.portfolioapp.databinding.FragmentTitleBinding
+
+/**
+ * A simple [Fragment] subclass.
+ */
+class TitleFragment : Fragment() {
+
+    override fun onCreateView(
+        inflater: LayoutInflater, container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View? {
+        val binding = DataBindingUtil.inflate<FragmentTitleBinding>(
+            layoutInflater, R.layout.fragment_title, container, false
+        )
+        return binding.root
+    }
+
+}
--- app/src/main/java/dev/irving/portfolioapp/trivia/TriviaScreen.kt
+++ app/src/main/java/dev/irving/portfolioapp/trivia/TriviaScreen.kt
@@ -1,6 +1,13 @@
 package dev.irving.portfolioapp.trivia
 
+import android.os.Bundle
 import androidx.appcompat.app.AppCompatActivity
+import dev.irving.portfolioapp.R
 
 class TriviaScreen : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.trivia_screen)
+    }
 }
