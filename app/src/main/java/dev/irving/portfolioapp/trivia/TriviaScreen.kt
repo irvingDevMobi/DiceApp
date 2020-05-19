70eb3b31310cd68ab89f1a2f380e2c606fe7e34f
Irving Lop <ilopezp@gbm.com.mx>
2020-05-19 18:27:54 -0500

    [Udacity] Lesson 3 App Navigation - 14. Adding support to Up Button

--- app/src/main/java/dev/irving/portfolioapp/trivia/TriviaScreen.kt
+++ app/src/main/java/dev/irving/portfolioapp/trivia/TriviaScreen.kt
@@ -2,6 +2,8 @@ package dev.irving.portfolioapp.trivia
 
 import android.os.Bundle
 import androidx.appcompat.app.AppCompatActivity
+import androidx.navigation.findNavController
+import androidx.navigation.ui.NavigationUI
 import dev.irving.portfolioapp.R
 
 class TriviaScreen : AppCompatActivity() {
@@ -9,5 +11,13 @@ class TriviaScreen : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.trivia_screen)
+
+        val navController = findNavController(R.id.myNavHostFragment)
+        NavigationUI.setupActionBarWithNavController(this, navController)
+    }
+
+    override fun onSupportNavigateUp(): Boolean {
+        val navController = findNavController(R.id.myNavHostFragment)
+        return navController.navigateUp()
     }
 }
