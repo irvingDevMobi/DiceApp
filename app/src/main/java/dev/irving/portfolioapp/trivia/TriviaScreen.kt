a1474486b1b22b2a773c1d210fbbb26b9cae2d54
Irving Lop <ilopezp@gbm.com.mx>
2020-06-02 16:59:28 -0500

    [Udacity] Lesson 3 App Navigation - 24. Navigation Drawer

--- app/src/main/java/dev/irving/portfolioapp/trivia/TriviaScreen.kt
+++ app/src/main/java/dev/irving/portfolioapp/trivia/TriviaScreen.kt
@@ -2,22 +2,33 @@ package dev.irving.portfolioapp.trivia
 
 import android.os.Bundle
 import androidx.appcompat.app.AppCompatActivity
+import androidx.drawerlayout.widget.DrawerLayout
 import androidx.navigation.findNavController
+import androidx.navigation.ui.AppBarConfiguration
 import androidx.navigation.ui.NavigationUI
 import dev.irving.portfolioapp.R
+import dev.irving.portfolioapp.databinding.TriviaScreenBinding
 
 class TriviaScreen : AppCompatActivity() {
 
+    private lateinit var binding: TriviaScreenBinding
+    private lateinit var drawerLayout: DrawerLayout
+    private lateinit var appBarConfiguration: AppBarConfiguration
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.trivia_screen)
+        binding = TriviaScreenBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+        drawerLayout = binding.drawerLayoutTrivia
 
         val navController = findNavController(R.id.myNavHostFragment)
-        NavigationUI.setupActionBarWithNavController(this, navController)
+        NavigationUI.setupActionBarWithNavController(this, navController, drawerLayout)
+        appBarConfiguration = AppBarConfiguration(navController.graph, drawerLayout)
+        NavigationUI.setupWithNavController(binding.navViewTrivia, navController)
     }
 
     override fun onSupportNavigateUp(): Boolean {
         val navController = findNavController(R.id.myNavHostFragment)
-        return navController.navigateUp()
+        return NavigationUI.navigateUp(navController, drawerLayout)
     }
 }
